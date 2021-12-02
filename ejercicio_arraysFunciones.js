ac8afb992e49f79c3e17bb467cb7c01c8a8408fc
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-12-02 11:08:50 +0100

    Create HTML and JV´s files and add orders to do

---
+++ ejercicio_arraysFunciones.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// PASOS A SEGUIR:
+
+// 1) Pide 6 números por pantalla al usuario y los meta en un array
+// 2) Tiene que mostrar el array entero con todos sus elementos en el cuerpo de la página y en la consola.
+// 3) También tenemos que sacar el array ordenado, mostrar los elementos ordenados
+// 4) Invierte su orden y mostrarlo
+// 5) Mostrar cuántos elementos tiene el array
+// 6) Hacer una búsqueda de un valor introducido por el usuario y que nos diga si existe en el array y que además, nos diga su índice, su posición:
